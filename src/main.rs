2783fa2033b72927f1254e1a1e58b1b7594ed743
Efim Aksenov <efim@inetss.com>
2019-11-06 20:37:42 +0700

    wip:

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,48 @@ use std::thread;
 use std::sync::mpsc;
 use std::sync::mpsc::Receiver;
 
+// todo: use state pattern
 fn main() {
     let config = Config::new();
 
     let workflow = Workflow::new(config);
 
     for state in workflow {
-        let message = match state {
-            State::Pomodoro(_) => "Time to work!",
-            State::ShortBreak(_) => "Short break.",
-            State::LongBreak(_) => "Long break.",
-        };
-        println!("{}", message);
+        println!("{}", state.start_message);
         let receiver = state.execute();
-        let Message::Timer(message) = receiver.recv().unwrap();
-        println!("{}", message)
+        let Message::Timer() = receiver.recv().unwrap();
+        print!("{}", state.end_message)
     }
 
 }
 
-enum Message {
-    Timer(String)
-}
-
 struct Config {
-    pomodoro: Duration,
-    short_break: Duration,
-    long_break: Duration,
+    pomodoro_duration: Duration,
+    short_break_duration: Duration,
+    long_break_duration: Duration,
+    pomodoro_start_msg: String,
+    pomodoro_end_msg: String,
+    short_break_start_msg: String,
+    short_break_end_msg: String,
+    long_break_start_msg: String,
+    long_break_end_msg: String,
 }
 
 impl Config {
     fn new() -> Config {
-        let pomodoro = Duration::new(25 * 60, 0);
-        let short_break = Duration::new(5 * 60, 0);
-        let long_break = Duration::new(25 * 60, 0);
+        let pomodoro_duration = Duration::new(25 * 60, 0);
+        let short_break_duration = Duration::new(5 * 60, 0);
+        let long_break_duration = Duration::new(25 * 60, 0);
         Config {
-            pomodoro,
-            short_break,
-            long_break,
+            pomodoro_duration,
+            short_break_duration,
+            long_break_duration,
+            pomodoro_start_msg: "Time to work!".to_string(),
+            pomodoro_end_msg: "Time to rest.".to_string(),
+            short_break_start_msg: "Short break.".to_string(),
+            short_break_end_msg: "Break is over.".to_string(),
+            long_break_start_msg: "Long break.".to_string(),
+            long_break_end_msg: "Break is over.".to_string(),
         }
     }
 }
@@ -66,14 +70,14 @@ impl Iterator for Workflow {
 
     fn next(&mut self) -> Option<State> {
        let next_state = if self.was_break {
-            State::Pomodoro(self.config.pomodoro)
+            State::new(self.config.pomodoro_duration, self.config.pomodoro_start_msg, self.config.pomodoro_end_msg)
        } else {
            if self.break_counter < 3 {
                self.break_counter += 1;
-               State::ShortBreak(self.config.short_break)
+            State::new(self.config.short_break_duration, self.config.short_break_start_msg, self.config.short_break_end_msg)
            } else {
                self.break_counter = 0;
-               State::LongBreak(self.config.long_break)
+            State::new(self.config.long_break_duration, self.config.long_break_start_msg, self.config.long_break_end_msg)
            }
        };
         self.was_break = !self.was_break;
@@ -81,29 +85,25 @@ impl Iterator for Workflow {
     }
 }
 
-trait Command {
-    fn execute(self) -> Receiver<Message>;
+struct State {
+    duration: Duration,
+    start_message: String,
+    end_message: String,
 }
 
-enum State {
-    Pomodoro(Duration),
-    ShortBreak(Duration),
-    LongBreak(Duration),
-}
+impl State {
+    fn new(duration: Duration, start_message: String, end_message: String) -> State {
+        State {duration, start_message, end_message}
+    }
 
-impl Command for State {
     fn execute(self) -> Receiver<Message> {
         let (sender, receiver) = mpsc::channel();
 
-        let (duration, message) = match self {
-            State::Pomodoro(duration) => (duration, "Time to rest."),
-            State::ShortBreak(duration) => (duration, "Break is over."),
-            State::LongBreak(duration) => (duration, "Break is over."),
-        };
+        let duration = self.duration.copy();
 
         thread::spawn(move || {
             thread::sleep(duration);
-            sender.send(Message::Timer(message.to_string())).unwrap();
+            sender.send(Message::Timer()).unwrap();
         });
 
         receiver
